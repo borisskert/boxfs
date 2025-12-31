3df40a52d04905eaff3cf53ef18ce62aba4772de
Boris Skert <git@borisskert.de>
2025-12-31 08:38:25 +0100

    Copy an empty directory from source to relative target (Windows)

--- src/test/java/de/borisskert/boxfs/filesystem/windows/FileSystemTest.java
+++ src/test/java/de/borisskert/boxfs/filesystem/windows/FileSystemTest.java
@@ -838,6 +838,35 @@ abstract class FileSystemTest {
                         assertThat(Files.isExecutable(dir)).isTrue();
                     }
                 }
+
+                @Nested
+                class CopyDirectoryToRelativeSimpleTarget {
+                    private Path target;
+
+                    @BeforeEach
+                    void setup() throws IOException {
+                        target = fs.getPath("copyoftestdir");
+                        Files.copy(dir, target);
+                    }
+
+                    @AfterEach
+                    void teardown() throws IOException {
+                        deleteRecursivelyIfExists(target);
+                    }
+
+                    @Test
+                    void shouldCopyDirectory() throws IOException {
+                        assertThat(Files.exists(target)).isTrue();
+                        assertThat(Files.isDirectory(target)).isTrue();
+                        assertThat(Files.notExists(dir)).isFalse();
+                        assertThat(Files.isRegularFile(dir)).isFalse();
+                        assertThat(Files.isHidden(dir)).isFalse();
+                        assertThat(Files.isSymbolicLink(dir)).isFalse();
+                        assertThat(Files.isReadable(dir)).isTrue();
+                        assertThat(Files.isWritable(dir)).isTrue();
+                        assertThat(Files.isExecutable(dir)).isTrue();
+                    }
+                }
             }
         }
 
